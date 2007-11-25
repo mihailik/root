e50df5e6df29a953e48ce3de2ca206dc21fa2ea8
SND\mihailik_cp <SND\mihailik_cp@81fbe566-5dc4-48c1-bdea-7421811ca204>
2007-11-25 17:54:34 +0000

    More initial tree...
    git-svn-id: https://httplistener.svn.codeplex.com/svn@1964 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ Internal.StateMachine/HttpContentReaderChunked.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mihailik.Net.Internal.StateMachine
+{
+    partial class HttpContentReader
+    {
+        enum ChunkedReaderState
+        {
+            ChunkSizeExpected,
+            ChunkSizeLFExpected,
+            ChunkData,
+            ChunkDataCRExpected,
+            ChunkDataLFExpected,
+            SkipHeadersExpected,
+            SkipHeaders,
+            SkipHeadersLFExpected,
+            FinishLFExpected,
+            Complete
+        }
+
+        ChunkedReaderState currentChunkedReaderState;
+        int chunkSize;
+        int chunkSizeDigitCount;
+        int chunkReadSize;
+        const int MaxChunkSize = 1024 * 1024 * 128;
+
+        IEnumerable<ArraySegment<byte>> ReadChunked(byte[] buffer, int offset, int length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Internal.StateMachine/HttpContentReaderConnectionClose.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mihailik.Net.Internal.StateMachine
+{
+    partial class HttpContentReader
+    {
+        private struct HttpContentReaderConnectionClose
+        {
+            public IEnumerable<ArraySegment<byte>> Read(byte[] buffer, int offset, int length)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
