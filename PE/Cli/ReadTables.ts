5004f74d76dd634e0d2f588f53a3b8e4eea1111a
mihailik <mihailik@gmail.com>
2012-11-01 22:53:31 +0000

    Multimodule assemblies are allowed.

--- PE/Cli/ReadTables.ts
+++ PE/Cli/ReadTables.ts
@@ -67,14 +67,16 @@ module Mi.PE.Cli {
 
         readTables(_module: ModuleDefinition, streams: ReadStreams, reader: Mi.PE.IO.BinaryReader, tables: any[][]) {
             if (!tables[TableKind.Module]
-                || tables[TableKind.Module].length!=1)
-                throw new Error("Exactly one module record is required in each binary CLR module.");
+                || tables[TableKind.Module].length<1)
+                throw new Error("At least one module record is required in each binary CLR module.");
 
             _module.generation = reader.readShort();
             _module.name = streams.readString(reader);
             _module.mvid = streams.readGuid(reader);
             _module.encId = streams.readGuid(reader);
             _module.encBaseId = streams.readGuid(reader);
+
+
         }
     }
 }
