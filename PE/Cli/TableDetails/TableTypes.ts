318fa7d66a660a7a1557aac9fb3e4ba4d1550c44
mihailik <mihailik@gmail.com>
2012-11-03 13:25:49 +0000

    TableTypes with all the metadata.

---
+++ PE/Cli/TableDetails/TableTypes.ts
@@ -0,0 +1,49 @@
+module Mi.PE.Cli.TableDetails {
+    export interface TableTypes {
+        [tableKind: number]: TableType;
+
+        Module: TableType; // 0x00
+        TypeRef: TableType; // 0x01
+        TypeDef: TableType; // 0x02
+        Field: TableType; // 0x04
+        MethodDef: TableType; // 0x06
+        Param: TableType; // 0x08
+        InterfaceImpl: TableType; // 0x09
+        MemberRef: TableType; // 0x0A
+        Constant: TableType; // 0x0B
+        CustomAttribute: TableType; // 0x0C
+        FieldMarshal: TableType; // 0x0D
+        DeclSecurity: TableType; // 0x0E
+        ClassLayout: TableType; // 0x0F
+        FieldLayout: TableType; // 0x10
+        StandAloneSig: TableType; // 0x11
+        EventMap: TableType; // 0x12
+        Event: TableType; // 0x14
+        PropertyMap: TableType; // 0x15
+        Property: TableType; // 0x17
+        MethodSemantics: TableType; // 0x18
+        MethodImpl: TableType; // 0x19
+        ModuleRef: TableType; // 0x1A
+        TypeSpec: TableType; // 0x1B
+        ImplMap: TableType; // 0x1C
+        FieldRVA: TableType; // 0x1D
+        Assembly: TableType; // 0x20
+        AssemblyProcessor: TableType; // 0x21
+        AssemblyOS: TableType; // 0x22
+        AssemblyRef: TableType; // 0x23
+        AssemblyRefProcessor: TableType; // 0x24
+        AssemblyRefOS: TableType; // 0x25
+        File: TableType; // 0x26
+        ExportedType: TableType; // 0x27
+        ManifestResource: TableType; // 0x28
+        NestedClass: TableType; // 0x29
+        GenericParam: TableType; // 0x2A
+        MethodSpec: TableType; // 0x2B
+        GenericParamConstraint: TableType; // 0x2C
+    }
+
+    export class TableType {
+        constructor (public name: string, public tableKind: number, public comments: string, ctor: any) {
+        }
+    }
+}
