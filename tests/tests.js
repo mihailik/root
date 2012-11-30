a587fddee842725742dd6aba6f86d77867ebd3f3
mihailik <mihailik@gmail.com>
2012-11-30 08:24:53 +0000

    Tests for DataDirectory.contains.

--- tests/tests.js
+++ tests/tests.js
@@ -1272,6 +1272,69 @@ var test_DataDirectory;
         }
     }
     test_DataDirectory.toString_0xCEF_0x36A = toString_0xCEF_0x36A;
+    function contains_default_0_false() {
+        var dd = new pe.DataDirectory(0, 0);
+        if(dd.contains(0) !== false) {
+            throw dd.contains(0);
+        }
+    }
+    test_DataDirectory.contains_default_0_false = contains_default_0_false;
+    function contains_default_64_false() {
+        var dd = new pe.DataDirectory(0, 0);
+        if(dd.contains(64) !== false) {
+            throw dd.contains(64);
+        }
+    }
+    test_DataDirectory.contains_default_64_false = contains_default_64_false;
+    function contains_default_minus64_false() {
+        var dd = new pe.DataDirectory(0, 0);
+        if(dd.contains(-64) !== false) {
+            throw dd.contains(-64);
+        }
+    }
+    test_DataDirectory.contains_default_minus64_false = contains_default_minus64_false;
+    function contains_lowerEnd_below_false() {
+        var dd = new pe.DataDirectory(10, 20);
+        if(dd.contains(9) !== false) {
+            throw dd.contains(9);
+        }
+    }
+    test_DataDirectory.contains_lowerEnd_below_false = contains_lowerEnd_below_false;
+    function contains_lowerEnd_equal_true() {
+        var dd = new pe.DataDirectory(10, 20);
+        if(dd.contains(10) !== true) {
+            throw dd.contains(10);
+        }
+    }
+    test_DataDirectory.contains_lowerEnd_equal_true = contains_lowerEnd_equal_true;
+    function contains_lowerEnd_above_true() {
+        var dd = new pe.DataDirectory(10, 20);
+        if(dd.contains(11) !== true) {
+            throw dd.contains(11);
+        }
+    }
+    test_DataDirectory.contains_lowerEnd_above_true = contains_lowerEnd_above_true;
+    function contains_lowerEndPlusSize_above_false() {
+        var dd = new pe.DataDirectory(10, 20);
+        if(dd.contains(31) !== false) {
+            throw dd.contains(31);
+        }
+    }
+    test_DataDirectory.contains_lowerEndPlusSize_above_false = contains_lowerEndPlusSize_above_false;
+    function contains_lowerEndPlusSize_equal_false() {
+        var dd = new pe.DataDirectory(10, 20);
+        if(dd.contains(30) !== false) {
+            throw dd.contains(30);
+        }
+    }
+    test_DataDirectory.contains_lowerEndPlusSize_equal_false = contains_lowerEndPlusSize_equal_false;
+    function contains_lowerEndPlusSize_below_true() {
+        var dd = new pe.DataDirectory(10, 20);
+        if(dd.contains(29) !== true) {
+            throw dd.contains(29);
+        }
+    }
+    test_DataDirectory.contains_lowerEndPlusSize_below_true = contains_lowerEndPlusSize_below_true;
 })(test_DataDirectory || (test_DataDirectory = {}));
 var TestRunner;
 (function (TestRunner) {
