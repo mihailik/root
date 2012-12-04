4d6e933ff6f2edec926c90b49f84cb3e370ef83f
mihailik <mihailik@gmail.com>
2012-12-04 07:19:24 +0000

    Correct references in pe.ts.

--- pe.ts
+++ pe.ts
@@ -1,4 +1,5 @@
 /// <reference path="headers/PEFile.ts" />
-/// <reference path="unmanaged/Import.ts" />
+/// <reference path="unmanaged/DllImport.ts" />
+/// <reference path="unmanaged/DllExport.ts" />
 /// <reference path="io.ts" />
 
