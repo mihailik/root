81926ba7f7c0fa614f8b831ecbacf8d5f29e0bf3
mihailik <mihailik@gmail.com>
2012-12-07 19:35:37 +0000

    Some rearrangements in TableStreamReader.

--- managed/tables/TableStreamReader.ts
+++ managed/tables/TableStreamReader.ts
@@ -11,19 +11,13 @@ module pe.managed.tables {
         readShort(): number { return this.baseReader.readShort(); }
         
         readString(): string {
-            var pos: number;
-            if(this.streams.strings.size<65535)
-                pos = this.baseReader.readShort();
-            else
-                pos = this.baseReader.readInt();
+            var pos = this.readPos(this.streams.strings.size);
 
             var result: string;
-            if(pos == 0 )
-            {
+            if(pos == 0 ) {
                 result = null;
             }
-            else
-            {
+            else {
                 result = this.stringHeapCache[pos];
 
                 if (!result) {
@@ -31,7 +25,7 @@ module pe.managed.tables {
                         throw new Error("String heap position overflow.");
 
                     var utf8Reader = this.baseReader.readAtOffset(this.streams.strings.address + pos);
-                    result = utf8Reader.readUtf8z(1024*1024*1024); // strings longer than 1GB? Not supported for security reasons.
+                    result = utf8Reader.readUtf8z(1024*1024*1024); // strings longer than 1GB? Not supported for a security excuse.
 
                     this.stringHeapCache[pos] = result;
                 }
@@ -41,17 +35,12 @@ module pe.managed.tables {
         }
 
         readGuid(): string {
-            var index: number;
-
-            if (this.streams.guids.length <= 65535)
-                index = this.baseReader.readShort();
-            else
-                index = this.baseReader.readInt();
+            var index = this.readPos(this.streams.guids.length);
 
             if (index == 0)
                 return null;
-
-            return this.streams.guids[(index-1)/16];
+            else
+                return this.streams.guids[(index-1)/16];
         }
         
         readBlob(): any { throw new Error("Not implemented."); }
@@ -60,5 +49,12 @@ module pe.managed.tables {
         readTypeDefOrRef(): any { throw new Error("Not implemented."); }
         
         readTableRowIndex(tableIndex: number): number { throw new Error("Not implemented."); }
+
+        private readPos(spaceSize: number): number {
+            if(spaceSize<65535)
+                return this.baseReader.readShort();
+            else
+                return this.baseReader.readInt();
+        }
     }
 }
