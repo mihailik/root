70dc4c39902327b41bd8295850109118f366d604
mihailik <mihailik@gmail.com>
2012-12-08 14:17:07 +0000

    Some more tests for TableStream.read/ModuleDefinition.

--- tests/testTableStream_read_sampleExe.ts
+++ tests/testTableStream_read_sampleExe.ts
@@ -75,7 +75,61 @@ module test_TableStream_read_sampleExe {
         var tas = new pe.managed.metadata.TableStream();
         tas.read(tbReader, mes);
 
-        if (tas.tables[pe.managed.metadata.TableTypes.Module.index][0].name !== "sample.exe")
-            throw tas.tables[pe.managed.metadata.TableTypes.Module.index][0].name;
+        var _module = tas.tables[pe.managed.metadata.TableTypes.Module.index][0];
+
+        if (_module.name !== "sample.exe")
+            throw _module.name;
+    }
+
+    export function modules_0_generation_0() {
+        var bi = new pe.io.BufferBinaryReader(sampleBuf);
+        var pef = new pe.headers.PEFile();
+        pef.read(bi);
+        var rvaReader = new pe.io.RvaBinaryReader(bi, pef.optionalHeader.dataDirectories[pe.headers.DataDirectoryKind.Clr].address, pef.sectionHeaders);
+
+        var cdi = new pe.managed.metadata.ClrDirectory();
+        cdi.read(rvaReader);
+
+        var cmeReader = rvaReader.readAtOffset(cdi.metadataDir.address);
+        var cme = new pe.managed.metadata.ClrMetadata();
+        cme.read(cmeReader);
+
+        var mes = new pe.managed.metadata.MetadataStreams();
+        mes.read(cdi.metadataDir.address, cme.streamCount, cmeReader);
+
+        var tbReader = cmeReader.readAtOffset(mes.tables.address);
+        var tas = new pe.managed.metadata.TableStream();
+        tas.read(tbReader, mes);
+
+        var _module = tas.tables[pe.managed.metadata.TableTypes.Module.index][0];
+
+        if (_module.generation !== 0)
+            throw _module.generation;
+    }
+
+    export function modules_0_mvid_0d9cc7924913ca5a188f769e27c2bc72() {
+        var bi = new pe.io.BufferBinaryReader(sampleBuf);
+        var pef = new pe.headers.PEFile();
+        pef.read(bi);
+        var rvaReader = new pe.io.RvaBinaryReader(bi, pef.optionalHeader.dataDirectories[pe.headers.DataDirectoryKind.Clr].address, pef.sectionHeaders);
+
+        var cdi = new pe.managed.metadata.ClrDirectory();
+        cdi.read(rvaReader);
+
+        var cmeReader = rvaReader.readAtOffset(cdi.metadataDir.address);
+        var cme = new pe.managed.metadata.ClrMetadata();
+        cme.read(cmeReader);
+
+        var mes = new pe.managed.metadata.MetadataStreams();
+        mes.read(cdi.metadataDir.address, cme.streamCount, cmeReader);
+
+        var tbReader = cmeReader.readAtOffset(mes.tables.address);
+        var tas = new pe.managed.metadata.TableStream();
+        tas.read(tbReader, mes);
+
+        var _module = tas.tables[pe.managed.metadata.TableTypes.Module.index][0];
+
+        if (_module.mvid !== "{0d9cc7924913ca5a188f769e27c2bc72}")
+            throw _module.mvid;
     }
 }
