9d5d6529bc23020569f2994d0076a2db453d8c64
mihailik <mihailik@gmail.com>
2012-12-22 23:08:39 +0000

    Marking Constant actually a singleton.

--- managed/metadata/tableRows/Constant.ts
+++ managed/metadata/tableRows/Constant.ts
@@ -4,6 +4,8 @@ module pe.managed.metadata {
 	// The Constant table is used to store compile-time, constant values for fields, parameters, and properties.
 	// [ECMA-335 para22.9]
 	export class Constant {
+		isSingleton = true;
+
 		internalReadRow(reader: TableStreamReader): void {
 			var type = reader.readByte();
 			var padding = reader.readByte();
